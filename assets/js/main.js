32b22befdafd035714252b60722ac55744397efc
Danielateresa <dany.t_87@hotmail.it>
2022-10-19 14:52:31 +0200

    Added prompt

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,41 +15,57 @@ const timerEl = document.querySelector('.timer');
 
 //genero una lista con 10 numeri casuali
 //la mia lista di numeri casuali è inizialmente vuota
-let numbersList = [];
-
+let pcGenList = [];
+let userList = [];
 //ciclo for per ripetere 5 volte la generazione dei numeri casuali da 0 a 10
-function numberGenerator(numbersList) {
+function numberGenerator(pcGenList) {
     for (let i = 0; i < 5; i++) {
 
         //la funzione mathrandom mi genera un numero tra 0 e 10
         const randomNumber = Math.floor(Math.random() * 10) + 1;
 
         //con push aggiungo un numero generato alla lista, ad ogni iterazione
-        numbersList.push(randomNumber);
+        pcGenList.push(randomNumber);
 
     }
-
+    return pcGenList
 }
-
+const GeneratedRandomPcList = numberGenerator(pcGenList);
 //inserisco il generatore di array numeri casuali in una funzone
-const GeneratedRandomNumbersList = numberGenerator(numbersList);
 
-console.log(numbersList);
-
-numbersEl.innerText = 'Osserva i seguenti numeri e carca di ricordarli ' + numbersList;
-
+console.log(pcGenList);
+numbersEl.innerText = 'Osserva i seguenti numeri e carca di ricordarli ' + pcGenList;
 
 //parte un timer di 30 secondi. 
 //Dopo 30 secondi i numeri scompaiono
-let seconds = 30;
+let seconds = 5;
 
 //il timer deve dimininuire i secondi a ogni secondo che passa
-//set interval ripete ogni tot secondi
-setInterval(() => {
+//set interval ripete ogni 1000 millisecondi (1 secondo) il decrementare dei 30 secondi
+const intervalId = setInterval(function () {
+    timerEl.innerText = seconds;
+    seconds--;
+    //se il countdown è pari a -1 (così vsualizzo anche l'1) ferma il ciclo setinterval e fai scomparire i numeri
+    if (seconds == -1) {
 
-}, interval);
+        timerEl.style.display = 'none';
+        numbersEl.style.display = 'none';
+        clearInterval(intervalId);
+        /*Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, 
+uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().*/
 
+        for (let i = 0; i < 5; i++) {
+            const userNumber = prompt('Inserisci il primo numero della serie');
+            userList.push(userNumber);
+        }
+    }
+}, 1000);
 
+console.log(userList);
+
+/* Dopo che sono stati inseriti i 5 numeri, il software dice 
+quanti e quali dei numeri da indovinare sono stati individuati.
+ */
 
 
 
